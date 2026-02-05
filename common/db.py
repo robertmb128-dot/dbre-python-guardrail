4531a175432b1d2f28f0109437dd1b0e8282d85f
RMBJR <254317281+robertmb128-dot@users.noreply.github.com>
2026-02-05 03:35:54 -0600

    Update db.py

--- common/db.py
+++ common/db.py
@@ -1,1 +1,11 @@
 
+# common/db.py
+class Database:
+    def __init__(self, config):
+        self.config = config
+
+    def connect(self):
+        pass
+
+    def query(self, sql, params=None):
+        pass
